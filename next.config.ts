f7ad451748a386dea76587a24d2f00edcd224c67
mohamad-ridwan <mr643062@gmail.com>
2024-12-25 11:58:10 +0700

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    // path: '**',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**'
+      },
+      {
+        protocol: 'http',
+        hostname: '**'
+      }
+    ],
+    deviceSizes: [320, 420, 768, 1024, 1200],
+    imageSizes: [16, 32, 48, 64, 96],
+    formats: ['image/webp']
+  }
 };
 
 export default nextConfig;
